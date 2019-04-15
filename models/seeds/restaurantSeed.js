a7d3c238a6f77fd797c302ee3a249fbb6784e8fb
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-15 17:46:40 +0800

    add restaurantSee&json

---
+++ models/seeds/restaurantSeed.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Restaurant = require('../restaurant');
+const RestaurantData = require('../restaurant.json');
+
+mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+console.log(db);
+
+db.on('error', () => {
+  console.log('db Seeder error');
+});
+
+db.once('open', () => {
+  const data = RestaurantData.results;
+  for (let i = 0; i < data.length; i++) {
+    Restaurant.create({
+      name: data[i].name,
+      name_en: data[i].name_en,
+      category: data[i].category,
+      image: data[i].image,
+      location: data[i].location,
+      phone: data[i].phone,
+      google_map: data[i].google_map,
+      rating: data[i].rating,
+      description: data[i].description
+    });
+  }
+  console.log('db Seeder ok');
+});
