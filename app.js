f8ae9a95f0f7ea843320bad228eada6da7968fa1
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-16 18:06:28 +0800

    Add edit page and update action

--- app.js
+++ app.js
@@ -39,8 +39,15 @@ app.get('/restaurants/:id', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurants) => {
     if (err) return console.error(err);
     return res.render('detail', { restaurants });
-  })
-})
+  });
+});
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  Restaurant.findById(req.params.id, (err, restaurants) => {
+    if (err) return console.error(err);
+    return res.render('edit', { restaurants });
+  });
+});
 
 
 
@@ -57,10 +64,30 @@ app.post('/restaurants', (req, res) => {
     rating: req.body.rating,
     description: req.body.description,
   });
-
   restaurant.save(err => {
     if (err) return console.error(err);
     return res.redirect('/');                        // 新增完成後，將使用者導回首頁
+  });
+});
+
+app.post('/restaurants/:id', (req, res) => {
+  Restaurant.findById(req.params.id, (err, restaurants) => {
+    if (err) return console.error(err);
+    restaurants.name = req.body.name,
+      restaurants.name_en = req.body.name_en,
+      restaurants.id = req.body.id,
+      restaurants.category = req.body.category,
+      restaurants.image = req.body.image,
+      restaurants.location = req.body.location,
+      restaurants.phone = req.body.phone,
+      restaurants.google_map = req.body.google_map,
+      restaurants.rating = req.body.rating,
+      restaurants.description = req.body.description,
+
+      restaurants.save(err => {
+        if (err) return console.error(err);
+        return res.redirect(`/restaurants/${req.params.id}`);
+      });
   });
 });
 
